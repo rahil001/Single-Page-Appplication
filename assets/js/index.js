547012867379ea41c789c6a46d3e8a6f4a6c76e3
rmanzar <rahil.manzar@paytm.com>
2018-02-26 20:51:46 +0530

    spa native javascript

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+(function () {
+
+    var products = [];
+    var filters = {};
+
+    var checkboxes = $('.all-products input[type="checkbox"]');
+
+    checkboxes.click(function () {
+
+        var that = $(this);
+        var spec = that.att
+    })
+
+})();
